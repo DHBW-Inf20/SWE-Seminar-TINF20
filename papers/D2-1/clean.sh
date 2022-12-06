1c471d9af41c30156cc78dfb0bcce52ec7451dc2
Raqhael <Raphael.sack@gmx.de>
2022-12-06 18:02:29 +0100

    some papers

---
+++ papers/D2-1/clean.sh
@@ -0,0 +1,23 @@
+# Temp Files löschen, da diese nicht mehr gebraucht werden
+rm *.aux
+rm *.acn
+rm *.acr
+rm *.alg
+rm *.bbl
+rm *.bcf
+rm *.blg
+rm *.glg
+rm *.glo
+rm *.gls
+rm *.ist
+rm *.lof
+rm *.log
+rm *.lol
+rm *.lot
+rm *.lop
+rm *.run.xml
+rm "*.synctex(busy)"
+rm *.synctex.gz
+rm *.tdo
+rm *.toc
+find . -name \*.aux -type f -delete
